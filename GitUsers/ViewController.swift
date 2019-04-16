73d7a4ffac184665fd45047c2dc9ca4b5c2c07a4
Raa <mail2rufat@yahoo.com>
2019-04-16 15:02:18 +0400

    Initial Commit

---
+++ GitUsers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUsers
+//
+//  Created by Rufat A.A. on 4/16/19.
+//  Copyright © 2019 Rufat A.A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
